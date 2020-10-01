8c8cfb518e194b02ed0d62cdfe696a94fc656aa6
dias <victordiaspalmeira@gmail.com>
2020-10-01 14:45:21 -0300

    criação de database local

---
+++ data_dump.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import glob, os
+import pickle as p
+import datetime
+
+def open_data(path):
+    os.chdir(path)
+    df_list = list()
+    for file in glob.glob("*.csv"):
+        df = pd.read_csv(file, encoding = "ISO-8859-1", sep=";|,|\t")
+        #df_list.append(df)
+        
+        df_list.append(df)
+       
+    return df_list
+
+if __name__ == "__main__":
+    df_list = open_data('./data')
+    cols = ['Sigla da Empresa', 'Número do Voo', 'Código Autorização (DI)', 'Tipo de linha', 'Aeroporto Origem', 'Aeroporto Destino', 
+            'Partida Prevista', 'Partida Real', 'Chegada Prevista', 'Chegada Real', 'Situação do Voo', 'Justificativa']
+    anac_df = pd.DataFrame(columns=cols)
+    for df in df_list:
+        df.reset_index()
+        df.columns = cols
+        for col in df.columns:
+            try:
+                df[col] = df[col].apply(lambda x: x.replace('"', ''))
+            except:
+                pass
+        #print(df.head())
+        anac_df = anac_df.append(df, ignore_index = True)
+
+    format = '%d/%m/%Y %H:%M'
+    date_cols = ['Partida Prevista', 'Partida Real', 'Chegada Prevista', 'Chegada Real']
+    for key in date_cols:
+        anac_df[key] = pd.to_datetime(anac_df[key], format=format, errors='coerce')
+        #date_time_obj = datetime.datetime.strptime(date, format)
+        #print(date_time_obj)
+        break
+    print(anac_df)
+    #anac_df.to_csv('anac_historico.csv', encoding = "ISO-8859-1")
+    p.dump(anac_df, open("data.p", "wb"))
