e4b6a7ecf822ba2e9eb496a046f4e62d0070359c
gustavo4passos <gustavo4passos@gmail.com>
2020-10-05 23:00:24 -0300

    Moves all graphs to the same page; creates inputs.py

--- callbacks.py
+++ callbacks.py
@@ -6,6 +6,7 @@ import plotly.express as px
 import re
 import pandas as pd
 
+
 @app.callback(
     Output('flight-depart-arrival-plot', 'figure'),
     [Input('empresa-select', 'value')],
@@ -28,8 +29,9 @@ def update_graph_depart_arrival(empname, origin, destination, start_date, end_da
         plot_df = plot_df[plot_df['Aeroporto Origem'] == origin]
     if(destination != None):
         plot_df = plot_df[plot_df['Aeroporto Destino'] == destination]
-    
+
     return px.scatter(plot_df, y=plot_df['Chegada Prevista'], labels={"index": "Partida Prevista"}, color='Sigla da Empresa')
+
 
 @app.callback(
     Output('flight-duration-plot', 'figure'),
@@ -53,11 +55,13 @@ def update_graph_flight_duration(empname, origin, destination, start_date, end_d
         plot_df = plot_df[plot_df['Aeroporto Origem'] == origin]
     if(destination != None):
         plot_df = plot_df[plot_df['Aeroporto Destino'] == destination]
-        
-    duracao_series = (plot_df['Chegada Real'] - plot_df['Partida Real']).div(pd.Timedelta('1H'))
+
+    duracao_series = (plot_df['Chegada Real'] -
+                      plot_df['Partida Real']).div(pd.Timedelta('1H'))
     return px.scatter(plot_df, y=duracao_series, labels={"index": "Partida Prevista", "y": "Duração em horas"}, color='Sigla da Empresa')
 
-@app.callback(    
+
+@app.callback(
     Output('flight-status-plot', 'figure'),
     [Input('empresa-select', 'value')],
     [Input('origem-select', 'value')],
@@ -68,8 +72,8 @@ def update_graph_flight_duration(empname, origin, destination, start_date, end_d
 def update_graph_flight_status(empname, origin, destination, start_date, end_date):
     start = dt.strptime(re.split('T| ', start_date)[0], '%Y-%m-%d')
     end = dt.strptime(re.split('T| ', end_date)[0], '%Y-%m-%d')
-    
-    plot_df = anac_df[anac_df['Sigla da Empresa'] == empname]
+
+    plot_df = anac_df
     plot_df = plot_df.loc[str(start):str(end)]
 
     if(empname != None):
@@ -79,4 +83,4 @@ def update_graph_flight_status(empname, origin, destination, start_date, end_dat
     if(destination != None):
         plot_df = plot_df[plot_df['Aeroporto Destino'] == destination]
 
-    return px.histogram(x=plot_df['Situação do Voo'])    
+    return px.histogram(x=plot_df['Situação do Voo'])
